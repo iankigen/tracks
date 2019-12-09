3a72f10f6274e8057e1b76b6518047d28465c64a
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-12-09 13:37:05 +0300

    chore: add tracks to installed apps

--- app/app/settings.py
+++ app/app/settings.py
@@ -30,6 +30,10 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+APPS = [
+    'tracks'
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+INSTALLED_APPS += APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
