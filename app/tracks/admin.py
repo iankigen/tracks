5682fd70b721dbba34bf617c901db04c8e740ac6
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-12-26 17:00:11 +0300

    chore: add track admin

--- app/tracks/admin.py
+++ app/tracks/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Track
 
-# Register your models here.
+
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'url', 'created_at', 'modified_at')
+    list_filter = ('created_at', 'modified_at')
+
+    class Meta:
+        model = Track
+
+
+admin.site.register(Track, TrackAdmin)
