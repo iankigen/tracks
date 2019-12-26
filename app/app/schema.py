85762315489b37c1df9ff25ec3fe59863fbce254
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-12-26 17:36:28 +0300

    chore: add create track mutation to base schema

--- app/app/schema.py
+++ app/app/schema.py
@@ -1,10 +1,14 @@
 import graphene
 
-from tracks.schema import Query as TracksQuery
+from tracks.schema import Query as TracksQuery, Mutation as TrackMutation
 
 
 class Query(TracksQuery, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(TrackMutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
