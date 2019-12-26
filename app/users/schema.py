2d7dcdaa1ee07aaa25a16fe6647f9a2157043719
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-12-26 18:20:38 +0300

    chore: add users objecttype and mutation

---
+++ app/users/schema.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+
+import graphene
+from graphene_django.types import DjangoObjectType
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+
+
+class CreateUser(graphene.Mutation):
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    user = graphene.Field(UserType)
+
+    def mutate(self, info, username, password, email):
+        user = get_user_model()(
+            username=username,
+            email=email
+        )
+        user.set_password(password)
+        user.save()
+        return CreateUser(user=user)
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
